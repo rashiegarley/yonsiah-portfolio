6ded41381304286d19b2fe751e756c825178686f
rashiegarley <rashiegarleyjr11@gmail.com>
2022-11-25 16:20:42 +0000

    initial commits

--- src/components/AuthorCard.js
+++ src/components/AuthorCard.js
@@ -21,11 +21,7 @@ export default function AuthorCard({isActive, setisActive}) {
               <div id="content" class="prose lg:prose-xl px-2">
                 <h1 class="text-center text-gray-800 text-4xl capitalize font-medium">Rashie Y. Garley</h1>
                 <small class="text-center block text-md text-gray-800">Student, Bibliophile and Writer</small>
-                <p class="text-justify text-lg text-gray-800">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Veritatis vero voluptates vitae rerum eos non laboriosam deleniti voluptatibus ipsum blanditiis
-                  expedita architecto tempore necessitatibus quisquam vel ipsam corporis, nemo repudiandae. Lorem, ipsum
-                  dolor sit amet consectetur adipisicing elit. Modi asperiores, amet omnis inventore aliquid nam. Autem
-                  obcaecati ut ipsa rem temporibus nostrum, quibusdam optio, itaque culpa ad impedit aperiam quasi.</p>
+                <p class="text-justify text-lg text-gray-800">I love coding and chilling... all thanks to Carlos Nah for making this happen, lol!</p>
                   <div className="p-6 max-w-full mx-auto">
                   <ul className="flex items-center justify-center mx-4px px-2 gap-4">
                   <li className="border border-[#B2B2B2] p-2 rounded-lg">
